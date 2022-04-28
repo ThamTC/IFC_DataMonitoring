7d63e82786f50dcb11f32031ed6b31479776bbfd
ThamTC <thamtc.ifc@gmail.com>
2022-04-28 10:55:34 +0700

    add file unsave

--- client/src/routers/sideNavRouter.js
+++ client/src/routers/sideNavRouter.js
@@ -1,6 +1,5 @@
 import RealTime from '../components/tables/RealTime.vue'
 import Statistic from '../components/tables/Statistic.vue'
-import Test from '../components/tables/Test.vue'
 
 export default [
     {
@@ -12,10 +11,5 @@ export default [
         path: "/statistic",
         nmae: "statistic",
         component: Statistic
-    },
-    {
-        path: "/test",
-        nmae: "test",
-        component: Test
     }
 ]
