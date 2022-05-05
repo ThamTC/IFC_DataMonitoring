605ba43288b4daae6606588117248cee4581876d
ThamTC <thamtc.ifc@gmail.com>
2022-05-05 13:59:25 +0700

    edit logger

---
+++ server/services/logger.js
@@ -0,0 +1,36 @@
+const { createLogger, format, transports, addColors } = require("winston");
+const { combine, timestamp, label, printf, prettyPrint, colorize } = format;
+const DailyRotateFile = require("winston-daily-rotate-file");
+const path = require("path");
+addColors({
+  info: "bold blue", // fontStyle color
+  warn: "italic yellow",
+  error: "bold red",
+  debug: "green",
+});
+const logger = (folder, filename) => {
+  return createLogger({
+    format: combine(
+      label({ label: "IFC log" }),
+      timestamp(),
+      prettyPrint(),
+    ),
+    transports: [
+      new transports.Console({}),
+      new DailyRotateFile({
+        colorize: true,
+        filename: path.join(
+          __dirname + "/../logs/",
+          folder,
+          filename + "_%DATE%.log"
+        ),
+        datePattern: "YYYY-MM-DD-HH",
+        zippedArchive: true,
+        maxSize: "100m",
+        maxFiles: "31d",
+      }),
+    ],
+  });
+};
+
+module.exports = logger;
